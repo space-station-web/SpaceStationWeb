5afa9d60737390f47780b48733943c260cc684a1
chocoboy08 <chocoboy0508@gmail.com>
2024-01-19 12:25:29 +0900

    feat: :sparkles: add layout
    레이아웃을 추가했습니다.

---
+++ src/components/commons/layout/Layout.presenter.tsx
@@ -0,0 +1,24 @@
+import { type ReactNode } from "react";
+import * as styled from "./Layout.styles";
+import LayoutHeader from "./header/LayoutHeader.container";
+
+interface LayoutProps {
+  children: ReactNode;
+  pathName: string;
+}
+
+function LayoutUI({ children, pathName }: LayoutProps): JSX.Element {
+  return (
+    <styled.Background>
+      <styled.BackgroundTopImg />
+      <styled.Radial />
+      <styled.BackgroundCircle />
+      <LayoutHeader />
+      {children}
+      {pathName.includes("Home") && <styled.Divider />}
+      {pathName.includes("Home") && <styled.BackgroundBottomImg />}
+    </styled.Background>
+  );
+}
+
+export default LayoutUI;
--- src/components/commons/layout/header/LayoutHeader.presenter.tsx
+++ src/components/commons/layout/header/LayoutHeader.presenter.tsx
@@ -1,13 +1,29 @@
-import * as L from "./LayoutHeader.styles";
+import * as Styled from "./LayoutHeader.styles";
+import Notification from "./Notification.container";
 
 export default function LayoutHeaderUI(): JSX.Element {
   return (
-    <L.Wrapper>
-      <L.UserIcon src="/common/icon/User.png" />
-      <L.Text>허거덩님</L.Text>
-      <L.Text>마이페이지 </L.Text>
-      <L.Icon src="/common/icon/alarm.png" />
-      <L.Icon src="/common/icon/list.png" />
-    </L.Wrapper>
+    <Styled.Wrapper>
+      <div style={{ marginRight: 95 }}>로고</div>
+
+      <Styled.NavBar>
+        <Styled.NavBarItem>오늘의 질문</Styled.NavBarItem>
+        <Styled.NavBarItem>오늘의 글감</Styled.NavBarItem>
+        <Styled.NavBarItem>우주 정거장</Styled.NavBarItem>
+        <Styled.NavBarItem>책방</Styled.NavBarItem>
+        <Styled.NavBarItem>글쓰기</Styled.NavBarItem>
+      </Styled.NavBar>
+      <Styled.UserBar>
+        <Styled.User>
+          <Styled.UserImg />
+          <div style={{ color: "#f0f0f0", lineHeight: "31px" }}>허거덩님</div>
+        </Styled.User>
+        <div style={{ marginRight: 67, color: "#f0f0f0", lineHeight: "31px" }}>
+          나의 우주선
+        </div>
+        <Notification />
+        <img src="/common/icon/icon_search.svg" />
+      </Styled.UserBar>
+    </Styled.Wrapper>
   );
 }
