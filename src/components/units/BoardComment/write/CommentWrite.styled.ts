20138f2525d662a15cf450ac72f551509cfd52f5
김동혁 <clfgnm9@naver.com>
2024-01-20 19:35:58 +0900

    fix:

--- src/components/units/BoardComment/write/CommentWrite.styled.ts
+++ src/components/units/BoardComment/write/CommentWrite.styled.ts
@@ -67,6 +67,7 @@ export const Submit = styled.div`
   background-image: url("/common/icon/submit(black).png");
   border-radius: 20px;
   cursor: pointer;
+  
 
   transition: background-image 0.5s ease; /* 트랜지션 속성 추가 */
 
