f331ac6b0e6c8c10c7c2df994cd2d9773596c883
begojara99 <pch990824@naver.com>
2024-01-24 22:28:47 +0900

    feat: 로그인 api 작업 1차, 비밀 번호 변경 api 작업 1차

--- src/components/units/aboutLogin/find/login/Login.container.tsx
+++ src/components/units/aboutLogin/find/login/Login.container.tsx
@@ -1,13 +1,58 @@
 import { useRouter } from "next/router";
+import { useState, type ChangeEvent, useEffect } from "react";
+import axios from 'axios';
 import LoginUI from "./Login.presenter";
 
 export default function Login(): JSX.Element {
   const router = useRouter();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   const onClickMoveFindPw = async (): Promise<void> => {
     await router.push("../../../../../../login/FindPassword");
   };
   const onClickMoveFindEm = async (): Promise<void> => {
     await router.push("../../../../../../login/FindEmail");
   };
-  return <LoginUI onClickMoveFindPw={onClickMoveFindPw} onClickMoveFindEm={onClickMoveFindEm} />;
+
+  const onChangeEmail = (event: ChangeEvent<HTMLInputElement>): void => {
+    setEmail(event.currentTarget.value);
+  };
+
+  const onChangePassword = (event: ChangeEvent<HTMLInputElement>): void => {
+    setPassword(event.currentTarget.value);
+  };
+
+  const handleLogin = async (): Promise<void> => {
+    try {
+      console.log("em :",email);
+      console.log("pw :",password);
+
+      const response = await axios.post(
+        '/login',
+        {
+          email,
+          password,
+        }
+      );
+      console.log("res", response);
+      // const accessToken = response.data.data.accessToken;
+      // localStorage.setItem('accessToken', accessToken);
+
+      await router.push('../../../../../../Home');
+      // window.location.replace('/');
+      console.log('로그인성공');
+    } catch (error) {
+      console.log('실패하였습니다', error);
+      
+    }
+  };
+
+  return <LoginUI 
+  onClickMoveFindPw={onClickMoveFindPw} 
+  onClickMoveFindEm={onClickMoveFindEm} 
+  onChangeEmail={onChangeEmail} 
+  onChangePassword={onChangePassword} 
+  handleLogin={handleLogin} />;
 }
