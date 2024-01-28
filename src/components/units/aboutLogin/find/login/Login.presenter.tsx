53e16f26677e6f63b6c2ba491bbe0e3d4a6c2554
begojara99 <pch990824@naver.com>
2024-01-28 15:42:58 +0900

    feat: 로그인, 회원가입 UI 배경 수정 및 이전 페이지로 돌아가기 버튼 구현

--- src/components/units/aboutLogin/find/login/Login.presenter.tsx
+++ src/components/units/aboutLogin/find/login/Login.presenter.tsx
@@ -5,6 +5,12 @@ export default function LoginUI(props: ILogin): JSX.Element {
   return (
     <>
       <F.Wrapper>
+        <F.Prev onClick={props.onClickMovePrev} src="/common/icon/Arrow 2.png" />
+        <F.Stack style={{ marginTop: 363, alignItems: "center" }}>
+          <div style={{ color: "rgba(200, 200, 200, 0.7)", fontSize: 30 }}>나의 생각이 정착할</div>
+          <div style={{ color: "rgba(200, 200, 200, 0.7)", fontSize: 55 }}>우주 정거장</div>
+          <div style={{ color: "rgba(200, 200, 200, 0.7)", fontSize: 15 }}>2023 Space Station</div>
+        </F.Stack>
         <F.Form>
           <F.InputTextForm>
             <F.Input 
@@ -26,6 +32,8 @@ export default function LoginUI(props: ILogin): JSX.Element {
             </F.UtilBox>
 
             <F.UtilBox>
+              <F.UtilText onClick={props.onClickMoveSignUp}>회원 가입</F.UtilText>
+              <F.NormText>|</F.NormText>
               <F.UtilText onClick={props.onClickMoveFindEm}>이메일</F.UtilText>
               <F.NormText>|</F.NormText>
               <F.UtilText onClick={props.onClickMoveFindPw}>
--- src/components/units/aboutLogin/find/login/Login.styles.ts
+++ src/components/units/aboutLogin/find/login/Login.styles.ts
@@ -1,25 +1,26 @@
 import styled from "@emotion/styled";
 
 export const Wrapper = styled.div`
-  background-position: top center;
-  background-image: url("/common/background.png");
-
+  width: 1186px;
+  position: relative; 
+  z-index: 1;
   display: flex;
-  justify-content: center;
-  
-  position: relative;
-
-  ::before {
-    position: absolute;
-    content: "";
-    top: 0px;
-    left: 0px;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, 0.4);
-  }
+  flex-direction: column;
+  align-items: center;
 `;
 
+export const Prev = styled.img`
+  position: absolute;
+  top: 300px; 
+  left: 0; 
+  width: 32px;
+  height: 32px;
+`;
+export const Stack = styled.div<{ $gap?: string }>`
+  display: flex;
+  flex-direction: column;
+  gap: ${(props) => props.$gap};
+`;
 export const Form = styled.div`
   z-index: 100;
   background-color: #1e1e1e;
@@ -32,9 +33,12 @@ export const Form = styled.div`
   flex-direction: column;
   align-items: center;
 
-  margin: 550px 0 300px 0;
+  margin: 50px 0 300px 0;
   padding: 70px;
 `;
+
+
+
 
 export const InputTextForm = styled.div`
   display: flex;
