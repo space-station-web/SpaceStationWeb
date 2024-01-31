89ece2493b92f25f4bb0c324e6125aedb409db47
김동혁 <clfgnm9@naver.com>
2024-01-31 15:18:10 +0900

    fix: :bug: layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,19 @@
 import type { AppProps } from "next/app";
 
 import { Global } from "@emotion/react";
-import { globalStyles } from "../src/components/commons/styles/globalStyles";
 import Layout from "../src/components/commons/layout/Layout.container";
+import { globalStyles } from "../src/components/commons/styles/globalStyles";
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <>
       <Global styles={globalStyles} />
-      <Layout>
-        
+
+      <div style={{ width: "100%", display: "flex", flexDirection: "column" }}>
+        <Layout>
           <Component {...pageProps} />
-        
-      </Layout>
+        </Layout>
+      </div>
     </>
   );
 }
