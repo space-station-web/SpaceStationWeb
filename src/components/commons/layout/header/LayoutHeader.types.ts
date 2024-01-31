3ef30f33d4b7ca0095903d8afda2530f56fd5ba5
begojara99 <pch990824@naver.com>
2024-01-31 16:19:46 +0900

    feat: 로그인 recoil 작업 2차

---
+++ src/components/commons/layout/header/LayoutHeader.types.ts
@@ -0,0 +1,9 @@
+import type { MouseEvent } from "react";
+
+export interface ILayoutHeader {
+  onClickLogin: (event: MouseEvent<HTMLDivElement>) => void;
+  onClickLogout: (event: MouseEvent<HTMLDivElement>) => void;
+  onClickSignUp: (event: MouseEvent<HTMLDivElement>) => void;
+
+  login: boolean;
+}
