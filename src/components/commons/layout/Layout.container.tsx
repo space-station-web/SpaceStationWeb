9f355742ba560cec8b278410952c209471bfd7c0
chocoboy08 <chocoboy0508@gmail.com>
2024-02-05 12:57:37 +0900

    fix: :bug: remove header in login page
    로그인과 회원가입 화면에서 헤더를 삭제했습니다

--- src/components/commons/layout/Layout.container.tsx
+++ src/components/commons/layout/Layout.container.tsx
@@ -1,5 +1,5 @@
 import { usePathname } from "next/navigation";
-import { type ReactNode } from "react";
+import { useEffect, useState, type ReactNode } from "react";
 import LayoutUI from "./Layout.presenter";
 
 interface LayoutProps {
@@ -8,8 +8,20 @@ interface LayoutProps {
 
 function Layout({ children }: LayoutProps): JSX.Element {
   const pathName = usePathname();
-
-  return <LayoutUI pathName={pathName}>{children}</LayoutUI>;
+  const [margin, setMargin] = useState("");
+  useEffect(() => {
+    switch (pathName) {
+      case "mypage":
+        setMargin("250px");
+      default:
+        setMargin("667px");
+    }
+  }, [pathName]);
+  return (
+    <LayoutUI pathName={pathName} margin={margin}>
+      {children}
+    </LayoutUI>
+  );
 }
 
 export default Layout;
--- src/components/commons/layout/Layout.presenter.tsx
+++ src/components/commons/layout/Layout.presenter.tsx
@@ -1,31 +1,27 @@
-import { useEffect, useState, type ReactNode } from "react";
+import { type ReactNode } from "react";
 import * as styled from "./Layout.styles";
 import LayoutHeader from "./header/LayoutHeader.container";
 
 interface LayoutProps {
   children: ReactNode;
   pathName: string;
+  margin: string;
 }
 
-function LayoutUI({ children, pathName }: LayoutProps): JSX.Element {
-  const [margin, setMargin] = useState("");
-  useEffect(() => {
-    switch (pathName) {
-      case "mypage":
-        setMargin("250px");
-      default:
-        setMargin("667px");
-    }
-  }, [pathName]);
+function LayoutUI({ children, pathName, margin }: LayoutProps): JSX.Element {
   return (
     <styled.Background>
-      {!pathName?.includes("boards") && <styled.BackgroundTopImg />}
+      {!(pathName.includes("boards") || pathName.includes("search")) && (
+        <styled.BackgroundTopImg />
+      )}
       <styled.Radial />
       <styled.BackgroundCircle $margin={margin} />
-      <LayoutHeader />
+      {!(pathName.includes("login") || pathName.includes("signUp")) && (
+        <LayoutHeader />
+      )}
       <styled.ChildWrapper>{children}</styled.ChildWrapper>
-      {pathName?.includes("home") && <styled.Divider />}
-      {(pathName?.includes("home") || pathName?.includes("mypage")) && (
+      {pathName.includes("home") && <styled.Divider />}
+      {(pathName.includes("home") || pathName.includes("mypage")) && (
         <styled.BackgroundBottomImg />
       )}
     </styled.Background>
