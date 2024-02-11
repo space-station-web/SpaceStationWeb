5f4ba58ccedb6b97b20093d983e56c8b1cd22cc8
chocoboy08 <chocoboy0508@gmail.com>
2024-02-11 14:50:20 +0900

    feat: :sparkles: add search page
    검색 결과 페이지 추가

--- src/components/commons/layout/Layout.presenter.tsx
+++ src/components/commons/layout/Layout.presenter.tsx
@@ -13,17 +13,17 @@ function LayoutUI({ children, pathName, margin }: LayoutProps): JSX.Element {
 
   return (
     <styled.Background>
-      {!(
-        safePathName.includes("boards") || safePathName.includes("search")
-      ) && <styled.BackgroundTopImg />}
+      {!(pathName?.includes("boards") || pathName?.includes("search")) && (
+        <styled.BackgroundTopImg />
+      )}
       <styled.Radial />
       <styled.BackgroundCircle $margin={margin} />
-      {!(safePathName.includes("login") || safePathName.includes("signUp")) && (
+      {!(pathName?.includes("login") || pathName?.includes("signUp")) && (
         <LayoutHeader />
       )}
       <styled.ChildWrapper>{children}</styled.ChildWrapper>
-      {safePathName.includes("home") && <styled.Divider />}
-      {(safePathName.includes("home") || safePathName.includes("mypage")) && (
+      {pathName?.includes("home") && <styled.Divider />}
+      {(pathName?.includes("home") || pathName?.includes("mypage")) && (
         <styled.BackgroundBottomImg />
       )}
     </styled.Background>
