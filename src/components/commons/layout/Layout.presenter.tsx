112a006c00401a405a74acf2ce502cd42a0635ad
김동혁 <clfgnm9@naver.com>
2024-02-11 16:45:43 +0900

    fix: :bug: layout 에러 수정

--- src/components/commons/layout/Layout.presenter.tsx
+++ src/components/commons/layout/Layout.presenter.tsx
@@ -13,17 +13,17 @@ function LayoutUI({ children, pathName, margin }: LayoutProps): JSX.Element {
 
   return (
     <styled.Background>
-      {!(pathName?.includes("boards") || pathName?.includes("search")) && (
-        <styled.BackgroundTopImg />
-      )}
+      {!(
+        safePathName.includes("boards") || safePathName.includes("search")
+      ) && <styled.BackgroundTopImg />}
       <styled.Radial />
       <styled.BackgroundCircle $margin={margin} />
-      {!(pathName?.includes("login") || pathName?.includes("signUp")) && (
+      {!(safePathName.includes("login") || safePathName.includes("signUp")) && (
         <LayoutHeader />
       )}
       <styled.ChildWrapper>{children}</styled.ChildWrapper>
-      {pathName?.includes("home") && <styled.Divider />}
-      {(pathName?.includes("home") || pathName?.includes("mypage")) && (
+      {safePathName.includes("home") && <styled.Divider />}
+      {(safePathName.includes("home") || safePathName.includes("mypage")) && (
         <styled.BackgroundBottomImg />
       )}
     </styled.Background>
