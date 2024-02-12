e9540adaa188462686a78fc8357b5953a1a8fa0b
김동혁 <clfgnm9@naver.com>
2024-02-13 01:22:37 +0900

    feat: :sparkles: 푸터 추가

---
+++ src/components/commons/layout/Footer/Footer.styled.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  width: 66%;
+  height: 8.07rem;
+  display: flex;
+  flex-direction: column;
+  padding: 30px 0 30px 0;
+`;
+
+export const TextContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+
+export const Text = styled.p`
+  font-size: 0.69rem;
+  color: #b4b4b4;
+`;
