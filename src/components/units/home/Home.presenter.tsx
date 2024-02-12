8e453c6251f5e3b9fc380bba82b4f225d981224f
김동혁 <clfgnm9@naver.com>
2024-02-13 03:39:09 +0900

    refactor: :lipstick: home 수정

--- src/components/units/home/Home.presenter.tsx
+++ src/components/units/home/Home.presenter.tsx
@@ -1,51 +1,61 @@
 import * as styled from "./Home.styles";
 import type { HomeProps } from "./Home.types";
 
-function HomeUI({ onClickMoveLogin, onClickMoveWrite }: HomeProps): JSX.Element {
+function HomeUI({
+  onClickMoveLogin,
+  onClickMoveWrite,
+}: HomeProps): JSX.Element {
   return (
     <styled.Wrapper>
-      <styled.Stack style={{ marginTop: 363, alignItems: "center" }}>
-        <div style={{ color: "#f7f7f7", fontSize: 30 }}>나의 생각이 정착할</div>
-        <div style={{ color: "#f7f7f7", fontSize: 55 }}>우주 정거장</div>
-        <div style={{ color: "#f7f7f7", fontSize: 15 }}>2023 Space Station</div>
+      <styled.Stack style={{ marginTop: 260, alignItems: "center" }}>
+        <div style={{ color: "#f7f7f7", fontSize: "1.15rem" }}>
+          나의 생각이 정착할
+        </div>
+        <div style={{ color: "#f7f7f7", fontSize: "2.11rem" }}>우주 정거장</div>
+        <div style={{ color: "#f7f7f7", fontSize: "0.56rem" }}>
+          2023 Space Station
+        </div>
       </styled.Stack>
       <styled.Stack
-        $gap="245px"
+        $gap="9.42rem"
         style={{ alignItems: "center", marginTop: 57 }}
       >
-        <styled.Group $gap="22px">
+        <styled.Group $gap="0.84rem">
           <styled.LoginBtn>가입</styled.LoginBtn>
           <styled.LoginBtn onClick={onClickMoveLogin}>로그인</styled.LoginBtn>
         </styled.Group>
-        <styled.Stack $gap="30px">
-          <styled.Group $gap="30px">
-            <styled.Card $width="786px" $height="370px">
-              <styled.Stack $gap="9px">
+        <styled.Stack $gap="1.1rem">
+          <styled.Group $gap="1.1rem">
+            <styled.Card $width="30.2rem" $height="14.2rem">
+              <styled.Stack $gap="0.34rem">
                 <styled.SubTitle>오늘의 글감</styled.SubTitle>
                 <styled.Title>몰입</styled.Title>
-                <styled.SubBody style={{ marginTop: 26, width: 311 }}>
+                <styled.SubBody style={{ marginTop: 26, width: "11.9rem" }}>
                   너무 어렵게 생각하지마요.
                   <br /> 몰입은 무언가에 빠지는 것. 무게가 어떻든
                   <br /> 당신이 좋아하고 있는 걸 써내려가면 될 거예요.
                 </styled.SubBody>
               </styled.Stack>
-              <styled.Group $gap="17px" style={{ alignSelf: "flex-end" }}>
+              <styled.Group $gap="0.65rem" style={{ alignSelf: "flex-end" }}>
                 <styled.ContentsBtn>모음집 확인</styled.ContentsBtn>
                 <styled.ContentsBtn>작성하기</styled.ContentsBtn>
               </styled.Group>
             </styled.Card>
-            <styled.Card $width="378px" $height="370px">
+            <styled.Card $width="14.5rem" $height="14.2rem">
               <styled.Stack $gap="9px">
                 <styled.SubTitle>나도 한번 써볼까?</styled.SubTitle>
                 <styled.Title>기록지</styled.Title>
               </styled.Stack>
-              <styled.ContentsBtn onClick={onClickMoveWrite} style={{ alignSelf: "flex-end" }}>
+              <styled.ContentsBtn
+                onClick={onClickMoveWrite}
+                style={{ alignSelf: "flex-end" }}
+              >
                 작성하기
               </styled.ContentsBtn>
             </styled.Card>
           </styled.Group>
           <styled.Group $gap="30px">
-            <styled.Card $width="378px" $height="370px">
+            <styled.Card $width="14.53rem" $height="14.2rem">
               <styled.Stack $gap="9px">
                 <styled.SubTitle>다른 사람은 어떤 생각을 할까?</styled.SubTitle>
                 <styled.Title>우주 정거장</styled.Title>
@@ -54,7 +64,7 @@ function HomeUI({ onClickMoveLogin, onClickMoveWrite }: HomeProps): JSX.Element 
                 보러가기
               </styled.ContentsBtn>
             </styled.Card>
-            <styled.Card $width="786px" $height="370px">
+            <styled.Card $width="30.2rem" $height="14.2rem">
               <styled.Stack $gap="9px">
                 <styled.SubTitle>오늘의 질문</styled.SubTitle>
                 <styled.Title
@@ -62,7 +72,9 @@ function HomeUI({ onClickMoveLogin, onClickMoveWrite }: HomeProps): JSX.Element 
                 >
                   제일 좋아하는 과일은 무엇인가요?
                 </styled.Title>
-                <styled.Balloon style={{ fontSize: 17, color: "#bdbdbd" }}>
+                <styled.Balloon
+                  style={{ fontSize: "0.654rem", color: "#bdbdbd" }}
+                >
                   로그인/회원가입을 하시면 오늘의 질문을 볼 수 있어요 !
                 </styled.Balloon>
               </styled.Stack>
@@ -72,7 +84,7 @@ function HomeUI({ onClickMoveLogin, onClickMoveWrite }: HomeProps): JSX.Element 
             </styled.Card>
           </styled.Group>
           <styled.Group $gap="30px">
-            <styled.Card $width="786px" $height="370px">
+            <styled.Card $width="30.2rem" $height="14.2rem">
               <styled.Stack $gap="9px">
                 <styled.SubTitle>다른 작가님들의</styled.SubTitle>
                 <styled.Title>책방</styled.Title>
@@ -81,7 +93,7 @@ function HomeUI({ onClickMoveLogin, onClickMoveWrite }: HomeProps): JSX.Element 
                 보러가기
               </styled.ContentsBtn>
             </styled.Card>
-            <styled.Card $width="378px" $height="370px">
+            <styled.Card $width="14.5rem" $height="14.2rem">
               <styled.Stack $gap="9px">
                 <styled.SubTitle>나도 작가에 도전해볼까?</styled.SubTitle>
                 <styled.Title>책 등록하기</styled.Title>
