ce1e46428c079399528f3905cb91cf46591350c1
chocoboy08 <chocoboy0508@gmail.com>
2024-02-13 16:26:39 +0900

    feat: :sparkles: add modal component
    마이페이지에서 사용될 모달 컴포넌트 추가

---
+++ src/components/units/mypage/modal/Modal.styles.ts
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+
+export const Background = styled.div`
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  top: -101px;
+  background-color: rgba(0, 0, 0, 0.7);
+  z-index: 100;
+`;
