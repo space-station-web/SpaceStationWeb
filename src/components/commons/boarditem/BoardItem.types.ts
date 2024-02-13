dd9e83ffec1bc1f57171d2bbbace0ef7c81ca94f
chocoboy08 <chocoboy0508@gmail.com>
2024-02-13 16:26:08 +0900

    feat: :sparkles: add board item component
    보관함에 나오는 글 개체 컴포넌트를 추가

--- src/components/commons/boarditem/BoardItem.types.ts
+++ src/components/commons/boarditem/BoardItem.types.ts
@@ -1,11 +1,13 @@
+import { PostProps } from "../../units/mypage/Mypage.types";
+
 export interface BoardItemProps {
   type: "writting" | "question";
-  writtingItem?: { img: string; author: string; title: string };
+  writtingItem?: PostProps;
   questionItem?: { number: number; question: string; answer: string };
 }
 export interface BoardItemUIProps {
   type: "writting" | "question";
-  writtingItem?: { img: string; author: string; title: string };
+  writtingItem?: PostProps;
   questionItem?: { number: number; question: string; answer: string };
   save: boolean;
   handleSaveClick: () => void;
