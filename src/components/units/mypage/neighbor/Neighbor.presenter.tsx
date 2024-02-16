2ec807659385470a3c3e1c5314e9771284581f59
chocoboy08 <chocoboy0508@gmail.com>
2024-02-16 12:12:12 +0900

    feat: :sparkles: add neighbor api
    이웃 조회, 추가, 삭제 api를 연결했습니다.

--- src/components/units/mypage/neighbor/Neighbor.presenter.tsx
+++ src/components/units/mypage/neighbor/Neighbor.presenter.tsx
@@ -2,33 +2,57 @@ import { useEffect } from "react";
 import * as styled from "./Neighbor.styles";
 import { NeighborUIprops } from "./Neighbor.types";
 
-function NeighborUI({ neighborList }: NeighborUIprops) {
+function NeighborUI({
+  neighborList,
+  myFollowList,
+  changeFollow,
+  handleClickBack,
+}: NeighborUIprops) {
   useEffect(() => {
     console.log(neighborList);
   });
   return (
     <styled.Wrapper>
-      <styled.Header>
-        <div>이웃 목록</div>
-        <div>{neighborList.length}명</div>
-      </styled.Header>
-      <styled.List>
-        {neighborList.map((item) => {
-          return (
-            <styled.NeighborItem
-              key={`neighbor-item-${item.user_id}-${item.follow_id}`}
-            >
-              <div style={{ display: "flex" }}>
-                <styled.ProfileImg />
-                <div>{item.follow_id}</div>
-              </div>
-              <styled.FollowBtn $follow={item.user_id === 1}>
-                {item.user_id === 1 ? "취소" : "구독"}
-              </styled.FollowBtn>
-            </styled.NeighborItem>
-          );
-        })}
-      </styled.List>
+      <img
+        src="/common/icon/icon_back.svg"
+        style={{ alignSelf: "flex-start", cursor: "pointer" }}
+        onClick={handleClickBack}
+      />
+      <div>
+        <styled.Header>
+          <div>이웃 목록</div>
+          <div>{neighborList.length}명</div>
+        </styled.Header>
+        <styled.List>
+          {neighborList.map((item) => {
+            return (
+              <styled.NeighborItem
+                key={`neighbor-item-${item.user_id}-${item.follow_id}`}
+              >
+                <div
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    gap: 20,
+                    color: "#dcdcdc",
+                  }}
+                >
+                  <styled.ProfileImg />
+                  <div>{item.follow_nickname}</div>
+                </div>
+                <styled.FollowBtn
+                  $follow={myFollowList.includes(item.follow_id)}
+                  onClick={() => {
+                    changeFollow(item.follow_id);
+                  }}
+                >
+                  {myFollowList.includes(item.follow_id) ? "취소" : "구독"}
+                </styled.FollowBtn>
+              </styled.NeighborItem>
+            );
+          })}
+        </styled.List>
+      </div>
     </styled.Wrapper>
   );
 }
--- src/components/units/mypage/neighbor/Neighbor.types.ts
+++ src/components/units/mypage/neighbor/Neighbor.types.ts
@@ -1,9 +1,14 @@
 export interface NeighborItemProps {
   id: number;
   user_id: number;
+  nickname: string;
   follow_id: number;
+  follow_nickname: string;
 }
 
 export interface NeighborUIprops {
   neighborList: NeighborItemProps[];
+  myFollowList: number[];
+  changeFollow: (id: number) => void;
+  handleClickBack: () => void;
 }
