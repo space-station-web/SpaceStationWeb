b9f52f79cda0a235a4ea92e1fbfadef77eda62b5
chocoboy08 <chocoboy0508@gmail.com>
2024-02-18 02:43:03 +0900

    feat: :sparkles: add stored posts view
    보관된 글 확인 기능을 추가

--- src/components/units/mypage/Mypage.container.tsx
+++ src/components/units/mypage/Mypage.container.tsx
@@ -2,24 +2,29 @@ import axios from "axios";
 import { useRouter } from "next/router";
 import { ChangeEvent, useEffect, useState } from "react";
 import MyPageUI from "./Mypage.presenter";
-import { PostProps, QuestionProps } from "./Mypage.types";
+import { PostProps, QuestionProps, StorageProps } from "./Mypage.types";
 import type { NeighborItemProps } from "./neighbor/Neighbor.types";
 
 axios.defaults.baseURL = "http://localhost:8080/";
 
-export const STORAGEMOCK = [
-  { title: "공간" },
-  { title: "요리" },
-  { title: "하트" },
-];
 function MyPage() {
-  const [userId, setUserId] = useState("1");
+  const userId = "20";
+  const [accessToken, setAccessToken] = useState<string | null>("");
+  const [refreshToken, setRefreshToken] = useState<string | null>("");
   const router = useRouter();
   const idForSee = router.query.user_id as string;
   const [posts, setPosts] = useState<PostProps[]>([]);
   const [questions, setQuestions] = useState<QuestionProps[]>([]);
+  const [storages, setStorages] = useState<StorageProps[]>([]);
+  const [storedPosts, setStoredPosts] = useState<
+    { name: string; list: PostProps[]; id: number } | undefined
+  >(undefined);
+  const [clickedStorage, setClickedStorage] = useState({
+    delete: -1,
+    open: -1,
+  });
   const [followers, setFollowers] = useState<number[]>([]);
-  const [tabValue, setTabValue] = useState("내 글");
+  const [tabValue, setTabValue] = useState("보관함");
   const tabBarMenu =
     idForSee === userId ? ["내 글", "질문집", "보관함"] : ["내 글", "보관함"];
   const [deleteModalOpen, setDeleteModalOpen] = useState(false);
@@ -33,6 +38,7 @@ function MyPage() {
   const handleModalType = (type: string) => {
     setModalType(type);
   };
+
   const handleStorageChange = (e: ChangeEvent<HTMLInputElement>) => {
     setStorageName(e.target.value);
   };
@@ -42,6 +48,12 @@ function MyPage() {
     try {
       const response = await axios.get(
         `/posts/user/${idForSee}?limit=12&offset=0`,
+        {
+          headers: {
+            Authorization: "Bearer " + accessToken,
+            refresh: refreshToken,
+          },
+        },
       );
       setPosts(response.data.result);
     } catch {
@@ -50,8 +62,24 @@ function MyPage() {
   };
   const getQuestions = async (): Promise<void> => {
     try {
-      const response = await axios.get("/questions/my-answer");
-      console.log(response.data.result);
+      const response = await axios.get(
+        "/questions/my-answer?limit=12&offset=0",
+        {
+          headers: {
+            Authorization: "Bearer " + accessToken,
+            refresh: refreshToken,
+          },
+        },
+      );
+      await setQuestions(response.data.result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const getStorageList = async () => {
+    try {
+      const response = await axios.get(`/storagetype/user/${idForSee}`);
+      setStorages(response.data.result);
     } catch (error) {
       console.log(error);
     }
@@ -67,12 +95,39 @@ function MyPage() {
       console.log(error);
     }
   };
+  const getStoredPosts = async (): Promise<void> => {
+    try {
+      const response = await axios.get(
+        `/storages/my-post-storage?storageType=${clickedStorage.open}`,
+      );
+      setStoredPosts(
+        storages
+          .map((item) => item.storage_type_id)
+          .includes(clickedStorage.open)
+          ? {
+              list: response.data.result,
+              name: storages.filter((item) => {
+                if (item.storage_type_id === clickedStorage.open)
+                  return item.type;
+              })[0].type,
+              id: clickedStorage.open,
+            }
+          : undefined,
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
   //이웃 추가, 취소
   const changeFollow = async (): Promise<void> => {
-    const response = followers.includes(parseInt(idForSee))
-      ? await axios.delete("/follow", { data: { followId: idForSee } })
-      : await axios.post("/follow", { followId: idForSee });
-    getFollowList();
+    try {
+      const response = followers.includes(parseInt(idForSee))
+        ? await axios.delete("/follow", { data: { followId: idForSee } })
+        : await axios.post("/follow", { followId: idForSee });
+      await getFollowList();
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   // 이웃 목록 버튼 클릭
@@ -82,17 +137,52 @@ function MyPage() {
       query: { user_id: idForSee },
     });
   };
+  const handlePostClick = (id: number) => {
+    router.push(`/boards/${id}`);
+  };
+  const handleStorageSelect = (id: number, purpose: "delete" | "open") => {
+    if (purpose === "delete")
+      setClickedStorage({ ...clickedStorage, delete: id });
+    else {
+      setClickedStorage({ ...clickedStorage, open: id });
+    }
+  };
+  const handleBackClick = () => {
+    router.back();
+  };
   const addStorageItem = async () => {
-    const response = await axios.post("/storagetype", { type: storageName });
+    try {
+      const response = await axios.post("/storagetype", { type: storageName });
+      await getStorageList();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const deleteStorageItem = async () => {
+    try {
+      const response = await axios.delete(
+        `/storagetype/${clickedStorage.delete}`,
+      );
+      await getStorageList();
+    } catch (error) {
+      console.log(error);
+    }
   };
   useEffect(() => {
+    setAccessToken(localStorage.getItem("accessToken"));
+    setRefreshToken(localStorage.getItem("refreshToken"));
+
     if (idForSee) {
       getPosts();
-      getQuestions();
+      if (idForSee === userId) getQuestions();
+      getStorageList();
       getFollowList();
     }
   }, [idForSee]);
 
+  useEffect(() => {
+    getStoredPosts();
+  }, [clickedStorage.open, storages]);
   return (
     <MyPageUI
       tabValue={tabValue}
@@ -100,15 +190,22 @@ function MyPage() {
       tabBarMenu={tabBarMenu}
       posts={posts}
       questions={questions}
+      storages={storages}
+      storedPosts={storedPosts}
       deleteModalOpen={deleteModalOpen}
       handleDeleteModal={handleDeleteModal}
       modalType={modalType}
       handleModalType={handleModalType}
       handleNeighborClick={handleNeighborClick}
+      handlePostClick={handlePostClick}
+      handleStorageSelect={handleStorageSelect}
+      handleBackClick={handleBackClick}
       isMine={idForSee === userId}
       isFollowing={followers.includes(parseInt(idForSee))}
       handleFollowClick={changeFollow}
       storageName={storageName}
+      addStorageItem={addStorageItem}
+      deleteStorageItem={deleteStorageItem}
       handleStorageChange={handleStorageChange}
     />
   );
