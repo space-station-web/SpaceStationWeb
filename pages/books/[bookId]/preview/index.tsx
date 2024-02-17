6a72532e724b4beb8c8d6e5b90dbb38aa4935a5e
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-02-18 03:03:46 +0900

    docs: preview 위치 변경

---
+++ pages/books/[bookId]/preview/index.tsx
@@ -0,0 +1,5 @@
+import BookPreview from "../../../../src/components/units/Book/preview/BookPreview.container";
+
+export default function BookPreviewPage(): JSX.Element {
+  return <BookPreview />;
+}
--- pages/books/preview/index.tsx
+++
@@ -1,5 +0,0 @@
-import BookPreview from "../../../src/components/units/Book/preview/BookPreview.container";
-
-export default function BookPreviewPage(): JSX.Element {
-  return <BookPreview />;
-}
