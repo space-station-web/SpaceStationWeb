a68aadba4a685cfbfa2003c8f6e1df24314d496f
chocoboy08 <chocoboy0508@gmail.com>
2024-02-17 12:25:16 +0900

    feat: :sparkles: add search page ui
    검색페이지 UI 입니다.

---
+++ src/components/units/Search/SearchResult/SearchResult.styles.ts
@@ -0,0 +1,29 @@
+import styled from "@emotion/styled";
+import Back from "../../../../../public/common/icon/icon_back.svg";
+export const Wrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-left: 316px;
+  padding-right: 316px;
+  padding-top: 58px;
+`;
+export const IconBack = styled(Back)`
+  align-self: flex-start;
+  margin-left: 47px;
+`;
+export const Title = styled.div`
+  font-size: 30px;
+  color: #dcdcdc;
+`;
+export const SubTitle = styled.div`
+  font-size: 18px;
+  color: #8c8c8c;
+`;
+export const Results = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  gap: 45px;
+  margin-bottom: 204px;
+`;
