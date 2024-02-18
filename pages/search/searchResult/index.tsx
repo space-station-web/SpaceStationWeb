623d0df146ca29a34c14e2e85f6eba08c2e9ae9f
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-02-19 02:11:12 +0900

    fix: 빌드 버그 수정

--- pages/search/searchResult/index.tsx
+++
@@ -1,7 +0,0 @@
-import SearchResult from "../../../src/components/units/Search/SearchResult/SearchResult.container";
-
-function SearchResultPage() {
-  return <SearchResult />;
-}
-
-export default SearchResultPage;
